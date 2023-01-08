811b42a890fd4ce38531f3126ca27189ebd0ebcb
Ahmad Mujtaba <cadeta6@gmail.com>
2023-01-08 12:57:06 +0500

    updated

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "4dc9e30b52msh53ed92d97147bfap1a7a51jsn7ec3a25a2121",// enter your rapid api key here
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "8724b4f10adcd7647c81bc885496ae61"; // enter your key from openweather API
